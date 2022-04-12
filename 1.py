735f4919e65a49db6e17e6f1831af9b5c93096ad
Kirill Brodnikov <ctyd20@gmail.com>
2022-04-12 18:58:19 +0400

    fix small banner

--- 1.py
+++ 1.py
@@ -1,10 +1,14 @@
+
+first https://manifest.prod.boltdns.net/manifest/v1/hls/v4/clear/1107601866001/c07a77a9-124b-453e-9a8f-7d96a4bf17c1/10s/master.m3u8?fastly_token=NjI1NDg2YmVfZDFjNDBlZTBjYjRiNWVkMmMwYjZmNTE5ZWY2MzVkOGY4ZThlOWY0ZjA3MzQ1NzQwMWUxOTkzYWEzZWQ5ZDQwNA%3D%3D
+
+
 import urllib.request
 import os
 import shutil
 
 my_lessons = [
    #  http://vid.com/vod/mp4:vod/PRV/Yg0WGN_6.mp4/media_b180000_454.ts
-    "https://bcbolt446c5271-a.akamaihd.net/media/v1/hls/v4/clear/1107601866001/c07a77a9-124b-453e-9a8f-7d96a4bf17c1/54e8c63f-3631-4bf2-93cc-de56bf50504a/5x/segment21.ts?akamai_token=exp=1649706894~acl=/media/v1/hls/v4/clear/1107601866001/c07a77a9-124b-453e-9a8f-7d96a4bf17c1/54e8c63f-3631-4bf2-93cc-de56bf50504a/*~hmac=401d8ba6e7160b066636247158c070137a917cf829cb95a12d04b94fe392e45a" # replace this with your url 
+    "https://manifest.prod.boltdns.net/manifest/v1/hls/v4/clear/1107601866001/c07a77a9-124b-453e-9a8f-7d96a4bf17c1/54e8c63f-3631-4bf2-93cc-de56bf50504a/10s/rendition.m3u8?fastly_token=NjI1NDg2YzBfNTFlMTIwMzNlYTRkNjRmMjZjYjg4MTUzMjY0MzYwZjc4MjcwNjllOGZiZDI3MTFiZDljMGUyODYwOTMxZGMyYQ%3D%3D" # replace this with your url 
 
 
 ]
