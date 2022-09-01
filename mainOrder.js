9d028ec4f7ae5e3dfc448d167eab9eaad38c4bd3
Kyrylo Brodnicov <kyrylo.brodnikov@gmail.com>
2022-09-01 18:42:32 +0200

    edit order

--- mainOrder.js
+++ mainOrder.js
@@ -545,6 +545,9 @@ document.querySelectorAll('.count_element .plus').forEach((inputCount) => {
     let id = parseInt( this.parentElement.parentElement.getAttribute('id').replace('delete',''))
 
     let count = parseInt(this.value)+1
+    console.log(00000)
+    console.log(count)
+    console.log(00000)
     calculate_count(count, id);
     count_all_summ()
 
