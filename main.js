d08de035658e5927d54332db35159261e052c5e7
Kyrylo Brodnicov <kyrylo.brodnikov@gmail.com>
2022-11-09 20:39:41 +0100

    edit menu mobile

--- main.js
+++ main.js
@@ -191,7 +191,7 @@ function MoveMobileMenuWrapNext() {
 // menu
 
 document.querySelector('.button_menu').addEventListener('click', function(){
-console.log(this.classList.contains('.open'));
+console.log(document.querySelector('.main_slider_block').classList.contains('.open'));
     this.classList.toggle('open')
     document.querySelector('body').classList.toggle('overflow_html')
     
